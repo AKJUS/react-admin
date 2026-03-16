e9c7492ba5652395d5029b914862815748927355
Anibal Svarcas <anibal@marmelab.com>
2026-03-16 13:38:40 -0300

    Avoid double memoization in ArrayField

--- packages/ra-ui-materialui/src/field/ArrayField.tsx
+++ packages/ra-ui-materialui/src/field/ArrayField.tsx
@@ -1,17 +1,8 @@
-import * as React from 'react';
-import { ArrayFieldBase, type ArrayFieldBaseProps, genericMemo } from 'ra-core';
+import { ArrayFieldBase, type ArrayFieldBaseProps } from 'ra-core';
 
 import type { FieldProps } from './types';
 
-const ArrayFieldImpl = <
-    RecordType extends Record<string, any> = Record<string, any>,
->(
-    props: ArrayFieldProps<RecordType>
-) => <ArrayFieldBase {...props} />;
-
-ArrayFieldImpl.displayName = 'ArrayFieldImpl';
-
-export const ArrayField = genericMemo(ArrayFieldImpl);
+export const ArrayField = ArrayFieldBase;
 
 export interface ArrayFieldProps<
     RecordType extends Record<string, any> = Record<string, any>,
